b96b214cf8f2530cbe6eeb2274287ce52f2118b0
Aret Mimaryan <amimaryan@gmail.com>
2019-08-10 13:37:09 -0400

    Created LanguageController

---
+++ Controllers/LanguageController.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LanguageController : ControllerBase
+    {
+        // GET api/language/getlanguages
+        [HttpGet]
+        [Route("getlanguages")]
+        public ActionResult<string> GetLanguages()
+        { 
+            //TO-DO: Add Unity Container for Loosely Coupled Code.
+            var instance = new translatorapp.DomainLayer.TranslatorLogic();
+            var languages = instance.GetLanguages();
+            //TO-DO: Add Unity Container for Loosely Coupled Code.
+
+            var entries = languages.Select(d => string.Format("\"{0}\": \"{1}\"", d.LanguageCode, string.Join(",", d.Language)));      
+            var jsonEntries = "{" + string.Join(", ", entries) + "}";
+            return (jsonEntries);
+        }
+    }
+}
